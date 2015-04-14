30e168f745d7f50f4f53a6a69f3cb7b945fa6cca
kbz <kebingzao@gmail.com>
2015-04-14 15:13:47 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', routes);
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.get('/', function (req, res) {
+  // 如果请求中的 cookie 存在 isVisit, 则输出 cookie
+  // 否则，设置 cookie 字段 isVisit, 并设置过期时间为1分钟
+  if (req.cookies.isVisit) {
+    console.log(req.cookies);
+    res.send("visit again");
+  } else {
+    res.cookie('isVisit', 1, {maxAge: 60 * 1000});
+    res.send("first visit");
+  }
+});
+
+
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(4556);
+
+module.exports = app;
